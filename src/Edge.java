82aeeaf3185239d9cfdbbd90a4d33e8a01138b74
tomed21 <tomed21@gmail.com>
2019-05-13 22:29:56 -0500

    Opposite

--- src/Edge.java
+++ src/Edge.java
@@ -3,15 +3,6 @@ public class Edge
     Vertex origin = new Vertex();
     Vertex destination = new Vertex();
 
-    public Vertex endpoints()
-    {
-    }
-
-    public Vertex opposite(Vertex v)
-    {
-
-    }
-
     public Vertex getOrigin()
     {
         return origin;
@@ -30,5 +21,22 @@ public class Edge
     public void setDestination(Vertex destination)
     {
         this.destination = destination;
+    }
+
+    // returns both vertices
+    public Vertex endpoints(Vertex origin, Vertex destination)
+    {
+        return origin, destination;
+    }
+
+    // returns the other vertex that is not passed in as a parameter
+    // adapted from https://algs4.cs.princeton.edu/43mst/Edge.java.html
+    public Vertex opposite(Vertex v)
+    {
+        if (v == origin)
+            return destination;
+        else if (v == destination)
+            return origin;
+        else throw new IllegalArgumentException("Illegal vertex");
     }
 }
